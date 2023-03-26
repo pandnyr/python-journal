b2f3dc366bf5a2b19efb65fe5df70945fa49f598
Emin Ibrahimoglu <pandnyr@hotmail.com>
2023-03-26 18:10:38 +0300

    Inheritance Project

---
+++ inheritance_project.py
@@ -0,0 +1,37 @@
+class Website:
+    "parent"
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def loginInfo(self):
+        print(self.name + " "+ self.surname)
+
+
+#------------------------
+
+class Website1(Website):
+    "child"
+    def __init__(self, name, surname, ids):
+        Website.__init__(self, name, surname)
+     
+        self.ids = ids
+
+    def login(self):
+        print(self.name + " "+ self.surname + " " + self.ids)
+
+
+
+class Website2(Website):
+    def __init__(self, name, surname, email):
+        super().__init__(name, surname)
+        self.email = email
+
+    def login(self):
+        print(self.name + " " + self.surname + " " + self.email)
+
+
+p1 = Website("name","surname")
+p2 = Website1("name", "surname", "123")
+p3 = Website2("name"," surname", "mrbeast@gmail.com")
+p3.login()
