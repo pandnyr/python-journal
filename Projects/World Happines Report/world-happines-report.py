362782caeafb47baca251214e20692f30d172c0d
Emin Ibrahimoglu <pandnyr@hotmail.com>
2023-04-04 12:55:50 +0300

    new projects and bug fixes

--- Projects/World Happines Report/world-happines-report.py
+++ Projects/World Happines Report/world-happines-report.py
@@ -31,7 +31,23 @@ df.sort_values(['Happiness Rank Per Region'], ascending=True, inplace=True)
 
 plt.figure(figsize=(10,10))
 plt.bar(df['Region'], df['Happiness Rank Per Region'], align='center')
-plt.xlabel("Countries")
+plt.xlabel("Regions")
 plt.ylabel("Happines Score")
 plt.show()
 
+
+#------------------------
+
+data2015 = pd.read_csv('../python-journal/Projects/World Happines Report/archive/2015.csv')
+data2016 = pd.read_csv('../python-journal/Projects/World Happines Report/archive/2016.csv')
+data2017 = pd.read_csv('../python-journal/Projects/World Happines Report/archive/2017.csv')
+data2018 = pd.read_csv('../python-journal/Projects/World Happines Report/archive/2018.csv')
+data2019 = pd.read_csv('../python-journal/Projects/World Happines Report/archive/2019.csv')
+
+rows = []
+
+for happines in data2015['Happines Score'].unique():
+    happinessRankSum = data2015.loc[data2015['Happines Score'] == happines]['Happiness Rank'].sum()
+    avgHappinessScorePerRegion = happinessRankSum/happines
+    dict1 = {'Happines Score': happines, 'Happiness Rank Per Region': avgHappinessScorePerRegion, 'numCountriesInRegion': numCountriesInRegion};
+    rows_list.append(dict1)
